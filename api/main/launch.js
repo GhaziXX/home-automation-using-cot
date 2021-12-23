cca14bad2d37455723bc2a33090f946e917fa6f6
Ghazi Tounsi <gtounsi52@gmail.com>
2021-12-23 22:06:40 +0100

    add ocsp stapling

--- api/main/launch.js
+++ api/main/launch.js
@@ -6,6 +6,7 @@ const debug = require("debug")('phoenix:server');
 const tls = require('spdy'); // HTTP2 + HTTPS (HTTP2 over TLS)
 const fs = require('fs');
 let helmet = require('helmet');
+const ocsp = require('ocsp');
 
 const key_file = process.env.KEY_FILE || config["key-file"]
 const cert_file = process.env.CERT_FILE || config["cert-file"]
@@ -32,7 +33,27 @@ app.use(function(req,resp,next){
     }
 });
 
+var ocspCache = new ocsp.Cache();
 const server = tls.createServer(options, app);
+server.on('OCSPRequest', function(cert, issuer, callback) {
+    ocsp.getOCSPURI(cert, function(err, uri) {
+        if (err) return callback(error);
+        var req = ocsp.request.generate(cert, issuer);
+        var options = {
+            url: uri,
+            ocsp: req.data
+        };
+        ocspCache.request(req.id, options, callback);
+    });
+});
+var sslSessionCache = {};
+server.on('newSession', function(sessionId, sessionData, callback) {
+    sslSessionCache[sessionId] = sessionData;
+    callback();
+});
+server.on('resumeSession', function (sessionId, callback) {
+    callback(null, sslSessionCache[sessionId]);
+});
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, (error) => {
